1fffefe0567a7d9ac4abbbf3691a113e3b7a3f38
ciisiq <cii_3@hotmail.com>
2022-07-13 19:24:17 +0100

    show the cards on the screen and did some css

--- app.js
+++ app.js
@@ -8,33 +8,95 @@ let countLives = 3;
 playerLives.textContent = countLives;
 
 const getCards = () => [
-  { image: "images/panda1-01.png", name: "panda1" },
-  { image: "images/panda2-01.png", name: "panda2" },
-  { image: "images/panda3-01.png", name: "panda3" },
-  { image: "images/panda4-01.png", name: "panda4" },
-  { image: "images/panda5-01.png", name: "panda5" },
-  { image: "images/panda6-01.png", name: "panda6" },
-  { image: "images/padafront-01.png", name: "pandaFront" },
-  { image: "images/panda1-01.png", name: "panda1" },
-  { image: "images/panda2-01.png", name: "panda2" },
-  { image: "images/panda3-01.png", name: "panda3" },
-  { image: "images/panda4-01.png", name: "panda4" },
-  { image: "images/panda5-01.png", name: "panda5" },
-  { image: "images/panda6-01.png", name: "panda6" },
+  {
+    image: "images/panda1-01.png",
+    back: "images/pandafront-01.png",
+    name: "panda1",
+  },
+  {
+    image: "images/panda2-01.png",
+    back: "images/pandafront-01.png",
+    name: "panda2",
+  },
+  {
+    image: "images/panda3-01.png",
+    back: "images/pandafront-01.png",
+    name: "panda3",
+  },
+  {
+    image: "images/panda4-01.png",
+    back: "images/pandafront-01.png",
+    name: "panda4",
+  },
+  {
+    image: "images/panda5-01.png",
+    back: "images/pandafront-01.png",
+    name: "panda5",
+  },
+  {
+    image: "images/panda6-01.png",
+    back: "images/pandafront-01.png",
+    name: "panda6",
+  },
+  {
+    image: "images/panda1-01.png",
+    back: "images/pandafront-01.png",
+    name: "panda1",
+  },
+  {
+    image: "images/panda2-01.png",
+    back: "images/pandafront-01.png",
+    name: "panda2",
+  },
+  {
+    image: "images/panda3-01.png",
+    back: "images/pandafront-01.png",
+    name: "panda3",
+  },
+  {
+    image: "images/panda4-01.png",
+    back: "images/pandafront-01.png",
+    name: "panda4",
+  },
+  {
+    image: "images/panda5-01.png",
+    back: "images/pandafront-01.png",
+    name: "panda5",
+  },
+  {
+    image: "images/panda6-01.png",
+    back: "images/pandafront-01.png",
+    name: "panda6",
+  },
 ];
 
 const randomizeCards = () => {
   const cardData = getCards();
   //how to randominize array in JS:
   cardData.sort(() => Math.random() - 0.5);
-  console.log(cardData);
+  return cardData;
 };
 
-const backCard = document.createElement("div");
-const frontCard = document.createElement("div");
+const createCard = () => {
+  const cardData = randomizeCards();
 
-const displayCard = () => {
-  randomizeCards().map((d) => backCard.getCards.image);
+  cardData.forEach((e) => {
+    //create the cards on the html page
+    const card = document.createElement("div");
+    const backCard = document.createElement("img");
+    const frontCard = document.createElement("img");
+
+    card.classList = "card";
+    backCard.classList = "back";
+    frontCard.classList = "front";
+
+    frontCard.src = e.image;
+    backCard.src = e.back;
+
+    section.appendChild(card);
+    card.appendChild(frontCard);
+    card.appendChild(backCard);
+  });
 };
 
-displayCard();
+createCard();
